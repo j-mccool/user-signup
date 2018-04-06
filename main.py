a4a5862dceb90e5cb6e9cbcd0a853be3bc054473
jmccool <john.mccool@gmail.com>
2018-04-06 12:49:35 -0500

    finished validation; though not really utilizing regex; project finished enough to hit goals

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 import cgi
+import re
 
 app = Flask(__name__)
 
@@ -18,31 +19,44 @@ def welcome():
     e_error = ''
 
     #name validation
+    regex_contains = r"([\s])" 
+    #regex_email = r"([\w\-_]+)?\w+@{1}[\w]+\.{1}\w+"
+
+    def email_check(x):
+        y = 0
+
+        if x.count(' ') > 0:
+            y += 1
+        elif x.count('@') != 1:
+            y += 1
+        elif x.count('.') != 1:
+            y += 1
+        if y > 0:
+            return True
+
+    if re.search(regex_contains, new_user):
+        u_error = "Username can't contain spaces. Please re-enter."
+    if re.search(regex_contains, password):
+        p_error = "Password cannot contain spaces. Please re-enter."
+    if len(password) > 20 or len(password) < 3:
+        p_error = "Invalid password. Must be between 3 and 20 characters."
+    if len(new_user) > 20 or len(new_user) < 3:
+        u_error = "Invalid username. Must be between 3 and 20 characters."
     if new_user == '':
         u_error = "That's not a valid username. Value must not be null."
     if password == '':
         p_error = "Invalid password. Cannot be null."
-    if len(password) < 3:
-        p_error = "Invalid password. Must be between 3 and 20 characters, and cannot contain spaces."
-    elif len(password) > 20:
-        p_error = "Invalid password. Must be between 3 and 20 characters, and cannot contain spaces."
-    elif " " in password:
-        p_error = "Invalid password. Passwords cannot contain spaces."
-    if password_confirm == '':
-        pc_error = "Password confirmation must match password entered above. Please try again."
     if password_confirm != password:
-        pc_error = "Password and Confirmation must match. Please try again."
+        pc_error = "Passwords must match. Please re-enter."
     if email:
-        if len(email) < 3:
-            e_error = "Email must be between 3 and 20 characters."
-        if len(email) > 20:
-            e_error = "Email must be between 3 and 20 characters."
-        if email.count(".") != 1:
-            e_error = "Email must contain a single \'.\' and a single \'@\'. Please re-enter."
-        elif email.count("@") !=1:
-            e_error = "Email must contain a single \'.\' and a single \'@\'. Please re-enter."
-        if " " in email:
-            e_error = "Email cannot contain a space. Please re-enter."
+            if re.search(regex_contains, email):
+                e_error = "Email cannot contain spaces. Please re-enter."
+            #if not re.search(regex_email, email):
+            #    e_error = "Email must contain a single \'.\' and a single \'@\'. Please re-enter."
+            if email_check(email):
+                e_error = "Email must contain a single \'.\' and a single \'@\'. Please re-enter."
+            if len(email) > 20 or len(email) < 3:
+                e_error = "Email must be between 3 and 20 charactesr. Please re-enter."
 
     if not u_error and not p_error and not pc_error and not e_error:
         return render_template('welcome.html', username = new_user)
